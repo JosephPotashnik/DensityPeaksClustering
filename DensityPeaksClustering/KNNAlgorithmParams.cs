3f2127481f4b97a8245eeffa955c2fee22c248ff
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-02-28 18:30:12 +0000

    preparing KNNAlgorithmParams

---
+++ DensityPeaksClustering/KNNAlgorithmParams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DensityPeaksClustering
+{
+    public class KNNAlgorithmParams
+    {
+        public float[][] matrix { get; set; }
+        public int k { get; set; }
+
+        public override string ToString()
+        {
+            string s = $"k : {k}";
+            s += "matrix: ";
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    s += $"{matrix[i][j]} ";
+
+                }
+
+
+            }
+            return s;
+        }
+    }
+}
